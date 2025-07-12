85d48635e23b075980acd9194eaf764100b73773
Vraj3654 <148425141+Vraj3654@users.noreply.github.com>
2025-07-12 14:27:20 +0530

    gh

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
-dotenv.config();
-connectDB();
+const PORT = process.env.PORT || 4000;
 
 const app = express();
-app.use(cors());
+app.use(cors());    
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/items', require('./routes/itemRoutes'));
+app.get('/', (req, res) => res.send('ReWear API with Firebase'));
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use('/api/auth', require('./routes/auth'));
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
